247914392ef28c26ef3e854fd610e9ecc6553a12
Steven Maguire <stevenmaguire@gmail.com>
2023-08-01 09:37:43 -0500

    Add initial tooling

---
+++ main.py
@@ -0,0 +1,89 @@
+import argparse, imaplib, email, os, re
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("host")
+parser.add_argument("username")
+parser.add_argument("password")
+parser.add_argument("-tf", "--target_folder", default='INBOX')
+parser.add_argument("-bf", "--buffer_folder")
+
+args = parser.parse_args()
+
+pattern_uid = re.compile(r'\d+ \(UID (?P<uid>\d+)\)')
+
+def parse_uid(data):
+    match = pattern_uid.match(data)
+    return match.group('uid')
+
+M = imaplib.IMAP4(args.host)
+M.login(args.username, args.password)
+
+target_folder = args.target_folder
+if target_folder and " " in target_folder:
+    target_folder = '"%s"' % target_folder
+
+buffer_folder = args.buffer_folder
+if buffer_folder and " " in buffer_folder:
+    buffer_folder = '"%s"' % buffer_folder
+
+response, folders = M.list()
+normalized_folders = list(map(lambda b: b.decode().split(' "/" ')[1], folders))
+
+if target_folder not in normalized_folders:
+    print('Target folder %s does not exist; quitting' % target_folder)
+    exit()
+
+M.select(target_folder)
+
+if buffer_folder and buffer_folder not in normalized_folders:
+    print('Buffer folder %s does not exist; creating' % buffer_folder)
+    M.create(buffer_folder)
+
+senders_file_path = 'accounts/%s.csv' % args.username
+if os.path.exists(senders_file_path):
+    file = open(senders_file_path, mode = 'r', encoding = 'utf-8-sig')
+    senders = file.read().splitlines()
+else:
+    print('Senders list %s not found; quitting' % senders_file_path)
+    exit()
+if not senders:
+    print('Senders list %s is empty; quitting' % senders_file_path)
+    exit()
+
+def delete_message_by_uid(msg_uid):
+    mov, data = M.uid('STORE', msg_uid , '+FLAGS', '(\Deleted)')
+    M.expunge()
+    print('\t' + '...deleted from target folder %s' % target_folder)
+
+def handle_messages(messages, keyword):
+    email_ids  = messages[0].split()
+
+    for email_id in email_ids:
+
+        resp, uid = M.fetch(email_id, "(UID)")
+        msg_uid = parse_uid(uid[0].decode('utf-8'))
+
+        resp, message = M.fetch(email_id, "(RFC822)")
+        original = email.message_from_bytes(message[0][1])
+        print(keyword, msg_uid, original['subject'], original['from'], original['date'])
+
+        if buffer_folder:
+            result = M.uid('COPY', msg_uid, '%s' % buffer_folder)
+            if result[0] == 'OK':
+                print('\t' + '...copied to buffer folder %s' % buffer_folder)
+                delete_message_by_uid(msg_uid)
+            else:
+                print('failed', result)
+                break
+        else:
+            delete_message_by_uid(msg_uid)
+
+for sender in senders:
+    keyword = '(FROM "%s")' % sender
+    (retcode, messages) = M.sort('REVERSE DATE', 'UTF-8', keyword)
+    if retcode == 'OK':
+        handle_messages(messages, keyword)
+
+M.close()
+exit()
